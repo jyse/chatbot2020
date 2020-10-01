64a2a04d9c972a2ddb3f79d2f362d0084e417164
Jessy The <jessy@brightfish.nl>
2020-10-01 09:08:23 +0200

    Day2Morning25mins - Trying render Userbubble after answer first question

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,39 @@ import ChatBotBubble from './ChatBotBubble';
 import UserBubble from './UserBubble';
 
 class App extends React.Component{
-  state = {userMessage: ''};
+  state = {
+    userMessage: '',
+    firstQuestionAsked: true,
+    firstQuestionAnswered: false,
+  };
+
+
 
   onButtonClick = async message => {
-    this.setState({userMessage: message});
+    console.log(message,'is message coming here?');
+    console.log(this.state.firstQuestionAnswered);
+    this.setState({
+      userMessage: message, 
+      firstQuestionAnswered: true
+    });
+    console.log(this.state.firstQuestionAnswered,'what is firstQuestionAnswered?');
   }
 
   render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <ChatBotBubble onClick={this.onButtonClick}/>
-        <UserBubble message={this.state.userMessage}/>
-      </div>
-    );
+      if (this.state.firstQuestionAsked) {
+        return (
+          <div className="ui container" style={{ marginTop: '10px' }}>
+            <ChatBotBubble onClick={this.onButtonClick}/>
+          </div>
+        )
+      }
+      if (this.state.firstQuestionAnswered) {
+        return (
+          <div className="ui container" style={{ marginTop: '10px' }}>
+            <UserBubble message={this.state.userMessage}/>
+          </div>
+        )
+      }
   }
 }
 
