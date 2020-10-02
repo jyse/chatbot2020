46e14b43c7a9f0a7bf4b2bc9f6a1df3fb40fe886
Jessy The <jessy@brightfish.nl>
2020-10-02 11:39:02 +0200

    Day3Morning60minsPart1 - Being able to show all questions asked by ChatBotBubble with map function in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import ChatBotBubble from './ChatBotBubble';
 import UserBubble from './UserBubble';
 
-class App extends React.Component{
-  state = {
-    userMessage: null,
-  };
 
-  onButtonClick = async message => {
+
+const onButtonClick = async message => {
     this.setState({
       userMessage: message, 
     });
   }
 
   
-  render() {
-
-      return (
+const App = () => {
+  const configConvo = [
+    'Did you take any action on sales?',  
+    'How many potential new clients did you add to your list today? Use numbers', 
+    'How many calls did you make today or reach outs through social media did you make today?',
+    'How many appointments or conversations did you have where you discovered someone his or her wants and needs?', 
+    'How many pitches or offers did you present today?',
+    'How many sales?', 
+    'How much did you make in sales?' 
+  ];
+    return (
         <div className="ui container" style={{ marginTop: '10px' }}>
-            <ChatBotBubble onClick={this.onButtonClick} configConvo={this.state.userMessage} />
+          {configConvo.map(question => (
+              <ChatBotBubble onClick={onButtonClick} configChatBotMsg={question}/>
+          ))}
         </div>
-      );
-    }
-  }
+    );
+}
 export default App;
--- src/components/ChatBotBubble.js
+++ src/components/ChatBotBubble.js
@@ -14,24 +14,16 @@ const getConversation = (configConvo) => {
 }
 
 const ChatBotBubble = props => {
-    const question = getConversation(props.configConvo);
-     return (
+    console.log(props, 'what is in props here?');
+    return (
         <div>
         <div className="ui message">
             <div className="header">
                 Chatbot2020
             </div>
-            <p> Hello there! Did you take any actions on sales today?</p>
-                <button onClick={(e) => onButtonClick('Yes', props)} className="ui green basic button">Yes</button>
-                <button onClick={(e) => onButtonClick('No', props)} className="ui red basic button" >No</button>
+            <p> Question: {props.configChatBotMsg}</p>
         </div>
-        {/* <div className="ui message">
-            <div className="header">
-                Chatbot2020
-            </div>
-            <p> Question 2: {question}</p>
-        </div>
-        </div>          */}
+        </div>         
     );
 };
 
